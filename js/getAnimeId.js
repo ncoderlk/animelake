f893949167d4c823a46fc7d4e174f6f432d7f0ff
ncoderlk <ncoderlk@gmail.com>
2022-12-02 05:37:44 +0530

    updated

--- js/getAnimeId.js
+++
@@ -1,9 +0,0 @@
-const getAnimeId = async (value) => {
-  const res = await fetch(
-    `https://gogoanime.consumet.org/Search?keyw=${value}`
-  );
-  const data = await res.json();
-  return data;
-};
-
-export default getAnimeId;
